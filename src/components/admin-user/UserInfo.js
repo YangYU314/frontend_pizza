776c0eedc43119cd925b452af7ec142f01c93b50
Liam Tang <tangwangjun123@gmail.com>
2020-07-31 23:19:00 +1000

    fix bug on popwindow

--- src/components/admin-user/UserInfo.js
+++ src/components/admin-user/UserInfo.js
@@ -1,23 +1,9 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
-import axiosInstance from "../../api/axiosInstance";
+import PopWindow from "./PopWindow";
 import "./admin-user.scss";
 
 export default function UserInfo(props) {
   const { users, pageUpdate, currentPage, pageSize } = props;
-
-  const handleDelete = (id) => {
-    axiosInstance({
-      url: `/user/${id}`,
-      method: "DELETE",
-    })
-      .then((res) => {
-        console.log("res.data", res);
-        pageUpdate(currentPage, pageSize);
-      })
-      .catch((err) => console.log("err", err));
-  };
 
   return (
     <>
@@ -26,9 +12,12 @@ export default function UserInfo(props) {
           <span>{user.name}</span>
           <span>{user.email}</span>
           <span>{user.phone}</span>
-          <span className="icon" onClick={() => handleDelete(user._id)}>
-            <FontAwesomeIcon icon={faTrashAlt} />
-          </span>
+          <PopWindow
+            id={user._id}
+            pageUpdate={pageUpdate}
+            currentPage={currentPage}
+            pageSize={pageSize}
+          />
         </div>
       ))}
     </>
