0bae340a981d56d5d238e1e812b3b9096a1cb011
Liam Tang <tangwangjun123@gmail.com>
2020-07-31 16:48:45 +1000

    fix bug in diy page, add css style on admin user to match

--- src/components/admin-user/Title.js
+++ src/components/admin-user/Title.js
@@ -4,7 +4,6 @@ import "./admin-user.scss";
 export default function UserInfo() {
   return (
     <>
-      <h3>Admin User</h3>
       <div className="admin-user-page__table-title">
         <span>Name</span>
         <span>Email</span>
